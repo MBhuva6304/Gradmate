96ab9aa15dcfe42ad33bf5a3bb71caaf21a64365
MBhuva6304 <bhuvamanan6304@gmail.com>
2026-04-24 16:49:29 -0700

    Removed Middlewares

--- campus/settings.py
+++ campus/settings.py
@@ -30,7 +30,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'users.middleware.PageTimingMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
